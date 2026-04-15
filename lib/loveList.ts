ec41d37ed5f05d983abc9991cdc1732588ee672a
RizDas <dkkrishabh@gmail.com>
2026-04-15 19:25:50 +0530

    lovelist update

--- lib/loveList.ts
+++ lib/loveList.ts
@@ -1,10 +1,150 @@
 export const loveList = [
   {
     id: 1,
-    text: "I love the way you smile when you're genuinely happy.",
+    text: "your infectious laugh.",
   },
   {
     id: 2,
-    text: "I love how your presence makes everything feel calm and right.",
+    text: "that look in your eyes",
+  },
+  {
+    id: 3,
+    text: "the way you hold my arm.",
+  },
+  {
+    id: 4,
+    text: "that 'you are mine' grip",
+  },
+  {
+    id: 5,
+    text: "when you can't hide a blush.",
+  },
+  {
+    id: 6,
+    text: "your constant banger ideas",
+  },
+  {
+    id: 7,
+    text: "that face. just... WOW",
+  },
+  {
+    id: 8,
+    text: "the way your voice sound",
+  },
+  {
+    id: 9,
+    text: "how you are my lucky charm",
+  },
+  {
+    id: 10,
+    text: "how effortlessly hot you are",
+  },
+  {
+    id: 11,
+    text: "your tiny, soft hands in mine",
+  },
+  {
+    id: 12,
+    text: "how you're always on my side",
+  },
+  {
+    id: 13,
+    text: "the self love energy",
+  },
+  {
+    id: 14,
+    text: "how you lean in to whisper",
+  },
+  {
+    id: 15,
+    text: "the way you call my name",
+  },
+  {
+    id: 16,
+    text: "how you listen to me yapping",
+  },
+  {
+    id: 17,
+    text: "your soft and warm skin",
+  },
+  {
+    id: 18,
+    text: "your cute irresistible cheeks",
+  },
+  {
+    id: 19,
+    text: "the way you care for me",
+  },
+  {
+    id: 20,
+    text: "the essence of your smell",
+  },
+  {
+    id: 21,
+    text: "your innocent curiosity 😏",
+  },
+  {
+    id: 22,
+    text: "how you pay complete attention to me.",
+  },
+  {
+    id: 23,
+    text: "your amazing confidence",
+  },
+  {
+    id: 24,
+    text: "the way you mess with me",
+  },
+  {
+    id: 25,
+    text: "when you call me 'mine'",
+  },
+  {
+    id: 26,
+    text: "the way you rest your head on me",
+  },
+  {
+    id: 27,
+    text: "the way you tie your hair",
+  },
+  {
+    id: 28,
+    text: "your soft, deep eyes",
+  },
+  {
+    id: 29,
+    text: "your messy, loose hair",
+  },
+  {
+    id: 30,
+    text: "how i can call you 'mine'",
+  },
+  {
+    id: 31,
+    text: "the way you listen to my stories",
+  },
+  {
+    id: 32,
+    text: "your commanding voice",
+  },
+  {
+    id: 33,
+    text: "how you tease me",
+  },
+  {
+    id: 34,
+    text: "our late-night talks",
+  },
+  {
+    id: 35,
+    text: "the way you make my day better",
+  },
+  {
+    id: 36,
+    text: "how you are so... you",
+  },
+  {
+    id: 37,
+    text: "the way you're my safe spot",
   },
 ];
